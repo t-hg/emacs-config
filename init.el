905ace2f90eef96c5103d86dc20030a013f36e91
Tobias Hort-Giess <tobias.hortgiess@gmail.com>
2023-07-26 07:39:54 +0200

    add init.el

---
+++ init.el
@@ -0,0 +1,30 @@
+;; GUI settings
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+
+;; Global settings
+(setq inhibit-startup-message t)
+(setq make-backup-files nil)
+
+;; Move customization variables to a separate file and load it
+(setq custom-file (locate-user-emacs-file "custom-vars.el"))
+(load custom-file 'noerror 'nomessage)
+
+;; Interactively DO things
+(setq ido-enable-flex-matching t)
+(setq ido-everywhere t)
+(setq ido-separator "\n")
+(ido-mode 1)
+
+;; Add Milkypostman’s Emacs Lisp Package Archive
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+(package-initialize)
+
+;; To install a package run
+;; M-x package-refresh-contents
+;; M-x package-install RET <package>
+
+;; Evil mode (VIM bindings)
+(require 'evil)
+(evil-mode 1)

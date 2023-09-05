16d250edcadbf842f7fa8c3d67d295e33c66aad3
Tobias Hort-Giess <tobias.hortgiess@gmail.com>
2023-09-05 07:50:13 +0200

    add evil

--- init.el
+++ init.el
@@ -22,12 +22,6 @@
 (setq custom-file (locate-user-emacs-file "custom-vars.el"))
 (load custom-file 'noerror 'nomessage)
 
-;; Interactively DO things
-(setq ido-enable-flex-matching t)
-(setq ido-everywhere t)
-(setq ido-separator "\n")
-(ido-mode 1)
-
 ;; Add Milkypostman’s Emacs Lisp Package Archive
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
@@ -38,8 +32,21 @@
   "Install packages"
   (interactive)
   (package-refresh-contents)
+  (package-install 'evil)
+  (package-install 'ido-completing-read+)
   (package-install 'smex))
 
+;; Interactively DO things
+(ido-mode 1)
+(setq ido-everywhere t)
+(require 'ido-completing-read+)
+(ido-ubiquitous-mode 1)
+
 ;; Smex is a M-x enhancement for Emacs. Built on top of Ido.
+(require 'smex)
 (global-set-key (kbd "M-x") 'smex)
 (global-set-key (kbd "M-X") 'smex-major-mode-commands)
+
+;; Evil Mode (VIM bindings)
+(require 'evil)
+(evil-mode 1)
